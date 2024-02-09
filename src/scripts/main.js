b18982fb5939b96a7a690d82df61b7a61db56d7b
Fabio Henrique <Farabio973@hotmail.com>
2024-02-08 21:23:58 -0300

    Added more sections

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,7 @@
 window.addEventListener("scroll", ()=> {
     let header = document.querySelector('#header');
-    header.classList.toggle('scroll-menu', window.scrollY > 200);
+    header.classList.toggle('scroll-menu', window.scrollY > 70);
+
+    let pageUp = document.querySelector('.pageUp');
+    pageUp.classList.toggle('showPageUp', window.scrollY > 200);
 });
